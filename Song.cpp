4face11bbffd893c35e2098f5c635b92254a69e4
janeielliston <60746332+janeielliston@users.noreply.github.com>
2020-04-21 20:58:02 -0400

    Create Song.cpp

---
+++ Song.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Song.h"
+
+using namespace std;
+		// output the song in the format:
+		// title, artist, album, year, length
+		// output to console
+		friend ostream& operator<<(ostream& os, const Song& song){
+            os << song.title << " " << song.artist << " " << song.album <<
+                " " << song.year << " " << song.length;
+        }
+        
+        //output to file
+		friend ofstream& operator<<(ofstream& os, const Song& song){
+            os << song.title << " " << song.artist << " " << song.album <<
+                " " << song.year << " " << song.length;
+        }
+
+		// input the song in the format:
+		// title, artist, album, year, length
+		friend istream& operator>>(istream& is, Song& song){
+            is >> song.title >> song.artist >> song.album >> song.year >> song.length;
+        }
+            
+		//input from file
+		friend fstream& operator>>(fstream& is, Song& song){
+            is >> song.title >> song.artist >> song.album >> song.year >> song.length;
+        }
+         
+		// compare two song objects for equality
+		friend bool operator==(const Song& lhs, const Song& rhs){
+            if (lhs.title == rhs.title && lhs.artist == rhs.artist)
+                return true;
+            else 
+                return false;
+            }
+
+		// constructors
+		Song::Song(){}
+
+		Song::Song(string title, string artist, string album, int length, int year){
+         this->title = title;
+		 this->artist = artist;
+		 this->album = album;
+		 this->length = length; // length in seconds
+		 this->year = year;
+        }
+
+		// set the song
+		void Song::set(string title, string artist, string album, int length, int year){
+         this->title = title;
+		 this->artist = artist;
+		 this->album = album;
+		 this->length = length; // length in seconds
+		 this->year = year;            
+        }
