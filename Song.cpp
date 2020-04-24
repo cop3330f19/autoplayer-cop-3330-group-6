81813d3da70652c5e90f0c7c8e4520501b7e1e17
janeielliston <60746332+janeielliston@users.noreply.github.com>
2020-04-24 19:49:03 -0400

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -1,35 +1,51 @@
+/**************************************************************
+ * Name of the file: Song.cpp                                 *
+ * Group 6: Ra'Caria Burgess, Janei Elliston,                 *
+ *          Michael Mondelice, Michael Parrish                *
+ * Date last edited: 4/24/2020                                *
+ * Purpose of Song.cpp: To determine that two songs are equal *
+ * if they have same name and artist.                         *                           
+ **************************************************************/
 #include <iostream>
 #include <string>
 #include <fstream>
 #include "Song.h"
 
 using namespace std;
-		// output the song in the format:
-		// title, artist, album, year, length
-		// output to console
-		friend ostream& operator<<(ostream& os, const Song& song){
-            os << song.title << " " << song.artist << " " << song.album <<
-                " " << song.year << " " << song.length;
-        }
-        
-        //output to file
-		friend ofstream& operator<<(ofstream& os, const Song& song){
+       /**************************************************************
+		output the song in the format:
+		title, artist, album, year, length
+		output to console
+        **************************************************************/
+	friend ostream& operator<<(ostream& os, const Song& song){
             os << song.title << " " << song.artist << " " << song.album <<
                 " " << song.year << " " << song.length;
         }
 
-		// input the song in the format:
-		// title, artist, album, year, length
-		friend istream& operator>>(istream& is, Song& song){
+        /**************************************************************
+         output to file
+         **************************************************************/
+	friend ofstream& operator<<(ofstream& os, const Song& song){
+            os << song.title << " " << song.artist << " " << song.album <<
+                " " << song.year << " " << song.length;
+        }
+        /**************************************************************
+		 input the song in the format:
+		 title, artist, album, year, length
+         ***************************************************************/
+	friend istream& operator>>(istream& is, Song& song){
             is >> song.title >> song.artist >> song.album >> song.year >> song.length;
         }
             
-		//input from file
+	/***************************************************************
+         input from file
+         ***************************************************************/
 		friend fstream& operator>>(fstream& is, Song& song){
             is >> song.title >> song.artist >> song.album >> song.year >> song.length;
         }
-         
-		// compare two song objects for equality
+        /*************************************************************** 
+		 compare two song objects for equality
+	 ***************************************************************/
 		friend bool operator==(const Song& lhs, const Song& rhs){
             if (lhs.title == rhs.title && lhs.artist == rhs.artist)
                 return true;
@@ -41,16 +57,18 @@ using namespace std;
 		Song::Song(){}
 
 		Song::Song(string title, string artist, string album, int length, int year){
-         this->title = title;
+                 this->title = title;
 		 this->artist = artist;
 		 this->album = album;
 		 this->length = length; // length in seconds
 		 this->year = year;
         }
+	/**************************************************************
+         set the song	                                                            
+	 **************************************************************/        
 
-		// set the song
 		void Song::set(string title, string artist, string album, int length, int year){
-         this->title = title;
+                 this->title = title;
 		 this->artist = artist;
 		 this->album = album;
 		 this->length = length; // length in seconds
