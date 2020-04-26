f0228864b8f689a2993e3c736251536b8d5032e0
kyB1001 <59378562+kyB1001@users.noreply.github.com>
2020-04-25 22:35:33 -0400

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -3,7 +3,7 @@
  * Group 6: Ra'Caria Burgess, Janei Elliston,                 *
  *          Michael Mondelice, Michael Parrish                *
  * Date last edited: 4/24/2020                                *
- * Purpose of Playlist.h: Is to manage the songs, by creating *
+ * Purpose of Playlist.cpp: Is to manage the songs, by creating *
  all of the normal functions of a playlist.                   *                           
  **************************************************************/
 
