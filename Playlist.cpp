2970dcf6636d012788f4cab01b6c6eeff29ab6ab
mparrishFAMU <61263623+mparrishFAMU@users.noreply.github.com>
2020-04-26 22:34:40 -0400

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -4,154 +4,242 @@
  *          Michael Mondelice, Michael Parrish                *
  * Date last edited: 4/24/2020                                *
  * Purpose of Playlist.cpp: Is to manage the songs, by        *
- creating all of the normal functions of a playlist.          *                           
+ * creating                                                   *
+ all of the normal functions of a playlist.                   *                           
  **************************************************************/
-
+#include <string>
+#include "Song.h"
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <fstream>
 #include "Playlist.h"
- 
-void Playlist::setName(std::string n){
-    name = n;
+#include "StringHelper.h"
+#include <stdlib.h>  
+
+using namespace std;
+
+Playlist::Playlist()
+{
+    songnumber = 0;
 }
- 
-std::string Playlist::getName(){
+
+Playlist::Playlist(string name)
+{
+    songnumber = 0;
+
+    this->title = name;
+    string song, artist,album, line;
+    int year, songLength;
+
+    ifstream file;
+    file.open((StringHelper::stou(name) + ".playlist").c_str());
+    while(getline(file,line))
+    {
+        Song s;
+        song = StringHelper::parse(line,',')[0];
+        artist = StringHelper::parse(line,',')[1];
+        album = StringHelper::parse(line,',')[2];
+        year = atoi(StringHelper::parse(line,',')[3].c_str());
+        song_length = atoi(StringHelper::parse(line,',')[4].c_str());
+
+        s.set(song,artist,album,song_length,year);
+        addSong(s);
+        writeToFile();
+    }
+
+}
+
+void Playlist::setTitle(string songTitle)
+{
+    this->title = songTitle;
+}
+
+string Playlist::getTitle() const
+{
     return name;
 }
- 
-void Playlist::addSong(Song song){
+
+//will take a Song object as a parameter and insert it into list.
+void Playlist::addSong(Song song)
+{
     playlist.push_back(song);
 }
- 
-bool Playlist::deleteSong(Song song){
-    bool has_song = false;
- 
- for(int i = 0; i < playlist.size(); i++)
-    if(song == playlist[i]){
-       playlist.erase(playlist.begin()+i);
-         has_song = true;
+
+//will take a Song object as a parameter and delete it list.
+bool Playlist:: deleteSong(Song & song)
+{   
+    for(int i = 0; i < playlist.size(); i++)
+    {
+        if(song == playlist[i])
+        {
+          playlist.erase(playlist.begin()+i);
+          return true;
         }
     }
-    
-    return has_song;
+    return false;
 }
- 
-Playlist Playlist::intersect(Playlist i_list){
-    Playlist intersect_list;
- 
- for(Song s: playlist){
-    for(Song i: i_list.playlist){
-       if(s == i){
-                intersect_list.addSong(s);
-                break;
-            }
-        }
-    }
- 
-  return intersect_list;
+
+// Adds song to current playlist
+Playlist operator+(Playlist & p ,Song & s)
+{
+    p.addSong(s);
+    return p;  
 }
- 
-Playlist Playlist::merge(Playlist m_list){
-    Playlist merge_list;
- 
-    for(Song s: playlist){
-        merge_list.addSong(s);
-    }
- 
-    for(Song s: m_list.playlist){
-        merge_list.addSong(s);
-    }
- 
-    return merge_list;
+
+  // concatenation of two playlist objects 
+Playlist operator+(Playlist & playlist, Playlist & p)
+{
+    vector<Song> s = p.getPlaylist();
+    Playlist p2 = playlist;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        p2.addSong(s[i]);
+    }  
+    return p2;
 }
- 
-void Playlist::play(){
- 
-  switch(play_mode){
-   case 'N':
-      if(now_playing < playlist.size()){
-         std::cout << "NOW PLAYING: " << playlist[now_playing] << std::endl;
-             now_playing++;
-            }
-            break;
-   case 'R':
-     std::cout << "NOW PLAYING: " << playlist[now_playing] << std::endl;
-       break;
-   case 'L':
-     if(now_playing < playlist.size()){
-        std::cout << "NOW PLAYING: " << playlist[now_playing] << std::endl;
-           now_playing++;  
-            }
-            
-        if(now_playing == playlist.size()){
-              now_playing = 0;
-            }
-            break;
-     default:
-        if(now_playing < playlist.size()){
-           std::cout << "NOW PLAYING: " << playlist[now_playing] << std::endl;
-             now_playing++;
-            }
-            break;
-    }
+//removing a song from a playlist
+Playlist operator-(Playlist & p , Song & song)
+{
+    p.deleteSong(song);
+    return p;  
 }
- 
-void Playlist::setMode(char mode){
-    //N = Normal
-    //R = Repeat
-    //L = Loop
-    char m = toupper(mode);
- 
-    if(m != 'N' && m != 'R' && m != 'L'){
+
+vector<Song> Playlist:: getPlaylist()
+{
+    return playlist;
+}
+
+// Intersection of two playlists will have not have duplicate songs
+Playlist Playlist::intersect(Playlist & p)
+{
+    vector<Song> s1 = this ->playlist;
+    vector <Song> s2 = p.getPlaylist();
+    Playlist p1 = this ->title;
+
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+       if(s1[i] == s2[i])
+       {
+           p1.appendToFile(s2[i]);
+       }
+    }
+    return p1;  
+}
+
+
+//creates a playlist that is a merge of two playlists.
+
+Playlist Playlist::merge(Playlist & p)
+{
+    Playlist p1 = this ->title;
+    vector<Song> v = p.getPlaylist();
+    for (int i = 0; i < v.size() ; i++)
+    {
+        p1.addSong(v[i]);
+        p1.appendToFile(v[i]);
+    }
+    return p1;
+
+}
+
+//finds what play mode is selected
+
+int Playlist::getPlayingMode()
+{
+    int count = 0;
+    if(mode == 0)
+    {
+        return count;
+        count++;
+    }
+    if(mode == 1)
+    {
+        return count;
+    }
+    if(mode == 2)
+    {
+        return count;
+    }
+
+    return count;
+}
+
+// play's one song from the play list starting at first index
+void Playlist::play()
+{
+    if(mode == 0 || mode == 2)
+    {
+        cout << playlist[songnumber] << endl;
+        songnumber++;
+    }
+    if(mode == 1)
+         cout << playlist[songnumber] << endl;
+    if (mode == 2 && songnumber == playlist.size())
+    {
+        songnumber = 0;
+    }
+    if (mode == 0 && songnumber == playlist.size())
+    {
+        cout << " END OF PLAYLIST" << endl;
         return;
-    }else {
-        play_mode = m;
+    }
+   
+}
+
+
+// sets player mode Normal Repeat Loop
+void Playlist::setMode(char playingMode)
+{
+    if(playingMode == 'N')
+        mode = 0;
+    if(playingMode == 'R')
+        mode = 1;
+    if(playingMode == 'L')
+        mode = 2;
+}
+
+void Playlist::appendToFile(Song song)
+{
+    ofstream file;
+    file.open((StringHelper::stou(title) + ".playlist").c_str(), ios::app);
+    file << song;
+    
+}
+void Playlist::writeToFile()
+{
+    ofstream file;
+    file.open((StringHelper::stou(title) + ".playlist").c_str());
+    for (int i = 0; i < playlist.size(); i++)
+    {
+       file << playlist[i];
     }
 }
- 
-std::ostream& operator<<(std::ostream& os, const Playlist& pList){
-    os << "PLAYLIST SONGS" << std::endl;
-    os << "--------------------------------" << std::endl;
-    for(Song s: pList.playlist){
-        os << s << std::endl;
-    }
-    os << "--------------------------------" << std::endl;
-    return os;
-};
- 
-Playlist operator+(Playlist p1, Playlist p2){
-    Playlist merge_list;
- 
-    for(Song s: p1.playlist){
-        merge_list.addSong(s);
-    }
-    for(Song s: p2.playlist){
-        merge_list.addSong(s);
-    }
- 
-    return merge_list;
-};
- 
-Playlist operator+(Playlist p, Song song){
-    Playlist plus_list;
- 
-    for(Song s: p.playlist){
-        plus_list.addSong(s);
-    }
- 
-    plus_list.playlist.push_back(song);
-    return plus_list;
-};
- 
-Playlist operator-(Playlist p, Song song){
-    Playlist d_list;
- 
-    for(Song s: p.playlist){
-        if(!(s == song)){
-            d_list = d_list+s;
-        }
-    }
- 
-    return d_list;
-};
+
+ostream& operator<<(ostream& os, const Playlist& playlist)
+{
+    for(int i = 0; i < playlist.playlist.size(); i++)
+   {
+    os << playlist.playlist[i] << endl;
+      
+       return os;
+   }
+   
+}
+
+ Playlist:: Playlist(const Playlist & p) 
+ {
+    playlist = p.playlist;
+ }
+
+void Playlist::printPlaylist()
+{
+    for(int i = 0; i < playlist.size(); i++)
+        cout << playlist[i] << endl;
+
+}
  
 Playlist::Playlist(){
         name = " ";
