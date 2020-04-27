46c2091a3880945c1956cc335349edc15dcb580a
mparrishFAMU <61263623+mparrishFAMU@users.noreply.github.com>
2020-04-26 23:52:49 -0400

    Update Playlist.cpp

--- Playlist.cpp
+++ Playlist.cpp
@@ -2,7 +2,7 @@
  * Name of the file: Song.cpp                                 *
  * Group 6: Ra'Caria Burgess, Janei Elliston,                 *
  *          Michael Mondelice, Michael Parrish                *
- * Date last edited: 4/24/2020                                *
+ * Date last edited: 4/26/2020                                *
  * Purpose of Playlist.cpp: Is to manage the songs, by        *
  * creating                                                   *
  all of the normal functions of a playlist.                   *                           
