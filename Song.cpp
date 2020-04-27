249bc013188d8c85e3ce51b24973ce61a7234c50
mparrishFAMU <61263623+mparrishFAMU@users.noreply.github.com>
2020-04-26 22:56:22 -0400

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -9,68 +9,84 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <string>
 #include "Song.h"
+#include <iomanip>
+#include <math.h>
 
 using namespace std;
-       /**************************************************************
-		output the song in the format:
-		title, artist, album, year, length
-		output to console
-        **************************************************************/
-	ostream& operator<<(ostream& os, const Song& song){
-            os << song.title << " " << song.artist << " " << song.album <<
-                " " << song.year << " " << song.length;
-        }
 
-        /**************************************************************
-         output to file
-         **************************************************************/
-	ofstream& operator<<(ofstream& os, const Song& song){
-            os << song.title << " " << song.artist << " " << song.album <<
-                " " << song.year << " " << song.length;
-        }
-        /**************************************************************
-		 input the song in the format:
-		 title, artist, album, year, length
-         ***************************************************************/
-	istream& operator>>(istream& is, Song& song){
-            is >> song.title >> song.artist >> song.album >> song.year >> song.length;
-        }
-            
-        /***************************************************************
-         input from file
-         ***************************************************************/
-		fstream& operator>>(fstream& is, Song& song){
-            is >> song.title >> song.artist >> song.album >> song.year >> song.length;
-        }
-        /*************************************************************** 
-		 compare two song objects for equality
-	 ***************************************************************/
-	 bool operator==(const Song& lhs, const Song& rhs){
-            if (lhs.title == rhs.title && lhs.artist == rhs.artist)
-                return true;
-            else 
-                return false;
-            }
+Song::Song()
+{
+    
+}
+Song::Song(string title, string artist)
+{
+    this->title = title;
+    this->artist = artist;
+}
 
-		// constructors
-		Song::Song(){}
+Song::Song(string title, string artist, string album, int length, int year)
+{
+    this->title = title;
+    this->artist = artist;
+    this->album = album;
+    this->length = length;
+    this->year = year;
+}
 
-		Song::Song(string title, string artist, string album, int length, int year){
-                 this->title = title;
-		 this->artist = artist;
-		 this->album = album;
-		 this->length = length; // length in seconds
-		 this->year = year;
-        }
-	/**************************************************************
-         set the song	                                                            
-	 **************************************************************/        
+void Song::set(string title, string artist, string album, int length, int year)
+{
+    this->title = title;
+    this->artist = artist;
+    this->album = album;
+    this->length = length;
+    this->year = year;
+}
 
-		void Song::set(string title, string artist, string album, int length, int year){
-                 this->title = title;
-		 this->artist = artist;
-		 this->album = album;
-		 this->length = length; // length in seconds
-		 this->year = year;            
-        }
+bool operator==(const Song& left, const Song& right)
+{
+    return (left.title == rhs.title) && (right.artist == right.artist);
+}
+
+ostream& operator<<(ostream& os, const Song& song)
+{
+    
+    os << song.title << endl << song.artist << endl
+        << song.album << ' ' << "(" << song.year << ")" << endl << "Play Time: " <<  song.length / 60 
+        << showpoint << fixed << ':'<< setw(2) << left << (song.length % 60) <<  endl;
+    
+    return os;
+}
+
+ofstream& operator<<(ofstream& os, const Song& song)
+{
+     os << song.title << "," << song.artist << ',' 
+     << song.album << ',' << song.year << ',' << song.length << endl; 
+
+    return os;
+}
+
+istream& operator>>(istream& is, Song& song)
+{
+    cout << "Song Details" << endl;
+    cout << "Title: ";
+    is >> song.title; 
+    cout << "Artist: ";
+    is >> song.artist;
+    cout << "Album: ";
+    is >> song.album;
+    cout << "Year";
+    is >> song.year;
+    cout << "Length (in seconds): ";
+    is >> song.length;
+    
+    return is;
+}
+
+fstream& operator>>(fstream& is, Song& song)
+{
+    is >> song.title >> song.artist >> song.album >> song.year >> song.length;
+
+    return is;
+}
