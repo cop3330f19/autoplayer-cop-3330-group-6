38854a4456bfc87cca4e508dc24a103436287020
janeielliston <60746332+janeielliston@users.noreply.github.com>
2020-04-27 00:23:49 -0400

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -27,50 +27,74 @@ class Playlist
    int songnumber;
 
    public:
-
-   // Constructors
+   /*******************************************************************************
+    * Constructors                                                                
+    ******************************************************************************/
    Playlist();
    Playlist(std::string);
    Playlist(const Playlist &p2);
 
    friend std::ostream& operator<<(std::ostream& os, const Playlist& playlist);
-   //output to file
+   /******************************************************************************
+    * output to file                                                             
+    ******************************************************************************/
    friend std::ofstream& operator<<(std::ofstream& os, const Playlist& playlist);
 
-   // input the playlist in the format:
-   // title
+   /****************************************************************************
+    * input the playlist in the format:
+    * title
+    ****************************************************************************/
    friend std::istream& operator>>(std::istream& is, Playlist& playlist);
-   //input from file
+   /****************************************************************************
+    *input from file                                
+    ****************************************************************************/
    friend std::fstream& operator>>(std::fstream& is, Playlist& playlist);
 
    void setTitle(std::string);
 
    std::string getTitle() const;
-   //adds song to playlist
+   /*****************************************************************************
+    * adds song to playlist
+    *****************************************************************************/
    void addSong(Song song);
-   //deletes song from playlist
+   /*****************************************************************************
+    * deletes song from playlist
+    *****************************************************************************/
    bool deleteSong(Song & song);
-   // Adds song to playlist current playlist
+   /*****************************************************************************
+    * Adds song to playlist current playlist
+    *****************************************************************************/
    Playlist friend operator+(Playlist &p ,Song &s);
-   // concatenation of the two playlist objects 
+   /*****************************************************************************
+    * concatenation of the two playlist objects 
+    *****************************************************************************/
    Playlist friend operator+(Playlist & playlist , Playlist & p2);
-   //removing a song(s) from a playlist
+   /*****************************************************************************
+    * removing a song(s) from a playlist
+    *****************************************************************************/
    Playlist friend operator-(Playlist &p , Song & song);
 
    
    Playlist intersect(Playlist & Playlist);
   
    Playlist merge(Playlist & Playlist);
-   
-   //plays next indexed song
+   /******************************************************************************
+    * plays next indexed song
+    ******************************************************************************/
    void play();
-   //sets the mode for play function 
+   /******************************************************************************
+    * sets the mode for play function 
+    ******************************************************************************/
    static void setMode(char);
-   //pull for checking play mode
+   /******************************************************************************
+    * pull for checking play mode
+    ******************************************************************************/
    static int getMode();
 
    std::vector<Song> getPlaylist();
-  // prints all songs in vector
+  /*******************************************************************************
+   * prints all songs in vector
+   *******************************************************************************/
   void printPlaylist();
 
   void appendToFile(Song song);
